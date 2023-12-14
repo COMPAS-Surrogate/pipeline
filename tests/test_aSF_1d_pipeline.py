8768d3a83b1773f83f3635d8c2db8cd58306b0c9
avi <avi.vajpeyi@gmail.com>
2023-12-15 09:38:07 +1300

    add example

--- tests/test_aSF_1d_pipeline.py
+++ tests/test_aSF_1d_pipeline.py
@@ -2,17 +2,23 @@ import subprocess, os
 import pandas as pd
 import numpy as np
 
+import numpy as np
+import sklearn.gaussian_process as gp
+
+from scipy.stats import norm
+from scipy.optimize import minimize
 from lnl_computer.cli.main import make_sf_table, batch_lnl_generation, combine_lnl_data, make_mock_obs
 from compas_python_utils.cosmic_integration.binned_cosmic_integrator.bbh_population import \
     generate_mock_bbh_population_file
 
-np.random.seed(42)
+from lnl_surrogate.models.sklearn_gp_model import SklearnGPModel
+
 
 LNL_CSV = "combined_lnl_data.csv"
 PARAM_CSV = "parameter_table.csv"
 COMPAS_H5 = "mock_COMPAS_output.h5"
 MOCK_OBS = "mock_obs.npz"
-MAX_PTS = 50
+MAX_PTS = 20
 
 # CLI commands
 DRAW_SAMPLES_CMD = "make_sf_table"
@@ -42,25 +48,124 @@ def generate_training_data(asf_csv, outdir):
     data = pd.read_csv(lnl_fn)
     data = data.sort_values(by=["aSF"])
     # plot data x = aSF, y = lnl and save to outdir
-    fig = data.plot(x="aSF", y="lnl")
-    fig.savefig(f"{outdir}/lnl_npts{len(data):00d}.png")
+    ax = data.plot.scatter(x="aSF", y="lnl")
+    ax.get_figure().savefig(f"{outdir}/lnl_npts{len(data):00d}.png")
     return data
 
 
-def train_and_evaluate_model(train_data, outdir):
+
+
+def update_parameter_table_with_acquisition_function(train_data, outdir):
     # acqistion function updates aSF-csv.. temporarily hardcodeed:
     # subprocess.call([DRAW_SAMPLES_CMD, "-p", "aSF", "-n", 10, "-f", f"{outdir}/{PARAM_CSV}"])
-    make_sf_table(parameters=["aSF"], n=10, fname=f"{outdir}/{PARAM_CSV}")
+    model = SklearnGPModel()
+    model.train(
+        inputs=train_data.aSF.values.reshape(-1,1),
+        outputs=train_data.lnl.values.reshape(-1,1)
+    )
+    new_asf = sample_next_hyperparameter(expected_improvement, model, train_data.lnl.values, greater_is_better=True, bounds=np.array([[0, 0.2]]), n_restarts=25)
+    # save new_asf to asf_csv
+    new_asf_df = pd.DataFrame({"aSF": new_asf})
+    new_asf_df.to_csv(f"{outdir}/{PARAM_CSV}", index=False)
+
+
+
+
+
+def expected_improvement(x, model, evaluated_loss, greater_is_better=False, n_params=1):
+    """ expected_improvement
+
+    Expected improvement acquisition function.
+
+    Arguments:
+    ----------
+        x: array-like, shape = [n_samples, n_hyperparams]
+            The point for which the expected improvement needs to be computed.
+        gaussian_process: GaussianProcessRegressor object.
+            Gaussian process trained on previously evaluated hyperparameters.
+        evaluated_loss: Numpy array.
+            Numpy array that contains the values off the loss function for the previously
+            evaluated hyperparameters.
+        greater_is_better: Boolean.
+            Boolean flag that indicates whether the loss function is to be maximised or minimised.
+        n_params: int.
+            Dimension of the hyperparameter space.
+
+    """
+
+    x_to_predict = x.reshape(-1, n_params)
+
+    mu, sigma = model._model.predict(x_to_predict, return_std=True)
+
+    if greater_is_better:
+        loss_optimum = np.max(evaluated_loss)
+    else:
+        loss_optimum = np.min(evaluated_loss)
+
+    scaling_factor = (-1) ** (not greater_is_better)
+
+    # In case sigma equals zero
+    with np.errstate(divide='ignore'):
+        Z = scaling_factor * (mu - loss_optimum) / sigma
+        expected_improvement = scaling_factor * (mu - loss_optimum) * norm.cdf(Z) + sigma * norm.pdf(Z)
+        expected_improvement[sigma == 0.0] == 0.0
+
+    return -1 * expected_improvement
+
+
+def sample_next_hyperparameter(acquisition_func, model, evaluated_loss, greater_is_better=True,
+                               bounds=[0, 0.2], n_restarts=25):
+    """ sample_next_hyperparameter
+
+    Proposes the next hyperparameter to sample the loss function for.
+
+    Arguments:
+    ----------
+        acquisition_func: function.
+            Acquisition function to optimise.
+        gaussian_process: GaussianProcessRegressor object.
+            Gaussian process trained on previously evaluated hyperparameters.
+        evaluated_loss: array-like, shape = [n_obs,]
+            Numpy array that contains the values off the loss function for the previously
+            evaluated hyperparameters.
+        greater_is_better: Boolean.
+            Boolean flag that indicates whether the loss function is to be maximised or minimised.
+        bounds: Tuple.
+            Bounds for the L-BFGS optimiser.
+        n_restarts: integer.
+            Number of times to run the minimiser with different starting points.
+
+    """
+    best_x = None
+    best_acquisition_value = 1
+    n_params = bounds.shape[0]
+
+    for starting_point in np.random.uniform(bounds[:, 0], bounds[:, 1], size=(n_restarts, n_params)):
+
+        res = minimize(fun=acquisition_func,
+                       x0=starting_point.reshape(1, -1),
+                       bounds=bounds,
+                       method='L-BFGS-B',
+                       args=(model, evaluated_loss, greater_is_better, n_params))
+
+        if res.fun < best_acquisition_value:
+            best_acquisition_value = res.fun
+            best_x = res.x
+
+    return best_x
+
+
 
 
 def test_lnl_pipeline(tmp_path):
+    np.random.seed(42)
     outdir = f"{tmp_path}/test_lnl_pipeline"
     os.makedirs(outdir, exist_ok=True)
-    init_npts = 10
+    init_npts = 2
     _setup(asf_csv=PARAM_CSV, outdir=outdir, init_npts=init_npts)
 
     npts = 0
     while (npts < MAX_PTS):
         data = generate_training_data(asf_csv=PARAM_CSV, outdir=outdir)
-        train_and_evaluate_model(train_data=data, outdir=outdir)
+        update_parameter_table_with_acquisition_function(train_data=data, outdir=outdir)
         npts = len(data)
