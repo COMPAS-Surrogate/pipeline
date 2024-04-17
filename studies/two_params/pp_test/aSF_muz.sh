e49e4ff3ae0483be01de652d7700b98519ade2e2
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-04-17 23:42:11 +1200

    update settings

---
+++ studies/two_params/pp_test/aSF_muz.sh
@@ -0,0 +1,10 @@
+setup_pp_test\
+  -n 100\
+  --outdir "out_aSF_muz"\
+  --compas_h5 "/fred/oz101/avajpeyi/COMPAS_DATA/h5out_5M.h5"\
+  -p "aSF" -p "muz"\
+  --duration 2.0\
+  --n_init 20\
+  --n_rounds 10\
+  --n_pts_per_round 20\
+  -a "pv" -a "ei"\
