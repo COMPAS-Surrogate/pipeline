49cd24adce1bfc43be1306a5c82075dfada0d125
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-04-17 21:00:18 +1200

    add muz correction

--- studies/one_param/pp_test/muz.sh
+++ studies/one_param/pp_test/muz.sh
@@ -2,7 +2,7 @@ setup_pp_test\
   -n 5\
   --outdir "out_muz"\
   --compas_h5 "/fred/oz101/avajpeyi/COMPAS_DATA/h5out_5M.h5"\
-  -p "muz"\
+  -p "mu_z"\
   --duration 2.0\
   --n_init 10\
   --n_rounds 4\
