8244729c6e0f15c533be0a802395bddcef7881fa
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-04-17 20:57:20 +1200

    add muz pp

--- src/pipeline/pp_test/templates/slurm_template.sh
+++ src/pipeline/pp_test/templates/slurm_template.sh
@@ -2,20 +2,13 @@
 #SBATCH --output={{LOG}}/job_%A_%a.out
 #SBATCH --error={{LOG}}/job_%A_%a.err
 #SBATCH --time=00:100:00
-#SBATCH --array=0-{NJOBS}
+#SBATCH --array=0-{{NJOBS}}
 #SBATCH --mem=6G
 #SBATCH --cpus-per-task=1
 
 
-read_list_from_file() {
-    local my_array=()
-    while IFS= read -r line; do
-        my_array+=("$line")
-    done < "$1" # Specify the file to read
-}
-
-gen_cmd=($(read_list_from_file {{GEN_CMD_FILE}}))
-analy_cmd=($(read_list_from_file {{ANALY_CMD_FILE}}))
+readarray -t gen_cmd < '{{GEN_CMD_FILE}}'
+readarray -t analy_cmd < '{{ANALY_CMD_FILE}}'
 
 echo "Mock cmd: ${gen_cmd[$SLURM_ARRAY_TASK_ID]}"
 echo "Surrogate cmd: ${analy_cmd[$SLURM_ARRAY_TASK_ID]}"
