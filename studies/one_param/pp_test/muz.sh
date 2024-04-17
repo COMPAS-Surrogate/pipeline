e7da45f5cbda3ba9575e01e066093d7be2c64cae
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-04-17 20:47:06 +1200

    add muz pp

---
+++ studies/one_param/pp_test/muz.sh
@@ -0,0 +1,10 @@
+setup_pp_test\
+  -n 5\
+  --outdir "out_aSF"\
+  --compas_h5 "/fred/oz101/avajpeyi/COMPAS_DATA/h5out_5M.h5"\
+  -p "muz"\
+  --duration 2.0\
+  --n_init 10\
+  --n_rounds 4\
+  --n_pts_per_round 5\
+  -a "pv" -a "ei"\
