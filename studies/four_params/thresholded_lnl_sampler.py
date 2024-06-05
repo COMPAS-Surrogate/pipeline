82c57d44e07fbc5448ef8839917a2f9ccafa599c
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-06-05 17:24:19 +1200

    add sampler

---
+++ studies/four_params/thresholded_lnl_sampler.py
@@ -0,0 +1,45 @@
+from lnl_surrogate import LnLSurrogate
+from pipeline.pp_test.make_pp_plot import make_pp_plot, PP_DATA
+import bilby
+
+import glob
+import os
+import numpy
+from tqdm.auto import tqdm
+from lnl_surrogate.cli import cli_build_surrogate
+from pipeline.cli import pp_test
+import sys
+
+
+def get_csv_paths(regex):
+    return glob.glob(regex)
+
+
+def main(base_dir, regex, threshold_amt):
+    csv = get_csv_paths(f"{base_dir}/{regex}")
+    print(f"Generating thresholded-lnl posteriors for {len(csv)} csv files")
+
+    if len(csv) == 0:
+        raise ValueError(f"No CSV files found with the given regex: {base_dir}/{regex}")
+
+    for c in tqdm(csv):
+        c = os.path.abspath(c)
+        outdir = os.path.join(os.path.dirname(c), "out_thresholded")
+        os.makedirs(outdir, exist_ok=True)
+        cli_build_surrogate(csv=c, model_type="gp", outdir=outdir, lnl_threshold=threshold_amt,
+                            label=f"thresh{threshold_amt}")
+        print(f"Saved thresholded lnl surrogate model to {outdir}")
+
+    pp_test(
+        results_regex=f"{base_dir}/*/out_thresholded/*_result.json",
+        cached_pp_fn=None,
+        filename=f"{base_dir}/thresholded_pp_plot.png"
+    )
+
+
+if __name__ == "__main__":
+    main(
+        base_dir=sys.argv[1],
+        regex=sys.argv[2],
+        threshold_amt=float(sys.argv[3])
+    )
