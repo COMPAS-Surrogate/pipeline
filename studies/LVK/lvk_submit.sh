03dcc2efe61b0ab16b845b3305b1055f0750fd15
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-06-27 17:54:05 +1200

    add lvk

--- studies/LVK/lvk_submit.sh
+++ studies/LVK/lvk_submit.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 #SBATCH --job-name=lvk50
-#SBATCH --output=log_lvk%j.out
-#SBATCH --error=log_lvk%j.err
+#SBATCH --output=logs/lvk%j.out
+#SBATCH --error=log/lvk%j.err
 #SBATCH --time=10:00:00
 #SBATCH --mem=40G
 #SBATCH --array=0-5
