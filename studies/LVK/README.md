63a4f87aff5edb29d1626d362dc4080158ac5e90
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-11-25 17:18:20 +1100

    add jeff params

---
+++ studies/LVK/README.md
@@ -0,0 +1,8 @@
+dict(mu_z=-0.154, sigma_0=0.546, aSF=0.001, dSF=4.76)
+
+
+mu0    =  0.035    --> [float]          location (mean in normal) at redshift 0 
+alpha = muz    = -0.23    --> [float]           redshift scaling/evolution of the location (alpha in Neijssel+19)
+sigma = sigma_0 = 0.39     --> [float]          Scale (variance in normal) at redshift 0 (sigma in Neijssel+19)
+sigma_z = 0.00     --> [float]          redshift scaling of the scale (variance in normal, 0 in Neijssel+19)
+alpha   = 0.00    --> [float]           shape (skewness, alpha = 0 retrieves normal dist as in Neijssel+19)
--- studies/LVK/plot_median_model.py
+++ studies/LVK/plot_median_model.py
@@ -2,16 +2,19 @@ import argparse
 from lnl_computer.cosmic_integration.mcz_grid import McZGrid
 
 
+# JEFF_PARMS = dict(mu_z=-0.154, sigma_0=0.546, aSF=0.001, dSF=4.76)
+
+
 def parse_args():
     parser = argparse.ArgumentParser(description="Plot median model")
     # get path to compas H5 file
     parser.add_argument("--compas-h5", type=str, help="Path to COMPAS H5 file", required=True)
     # get path to output fnme (give default name)
     parser.add_argument("--out", type=str, help="Path to output file", default="median_model.png")
-    parser.add_argument("--aSF", type=float, help="aSF", default=0.014)
-    parser.add_argument("--dSF", type=float, help="dSF", default=5)
-    parser.add_argument("--mu_z", type=float, help="mu_z", default=-0.2)
-    parser.add_argument("--sigma_z", type=float, help="sigma_z", default=0.6)
+    parser.add_argument("--aSF", type=float, help="aSF", default=0.001)
+    parser.add_argument("--dSF", type=float, help="dSF", default=4.76)
+    parser.add_argument("--mu_z", type=float, help="mu_z", default=-0.154)
+    parser.add_argument("--sigma_0", type=float, help="sigma_0", default=0.546)
     return parser.parse_args()
 
 
@@ -36,7 +39,7 @@ def main():
         "aSF": args.aSF,
         "dSF": args.dSF,
         "mu_z": args.mu_z,
-        "sigma_z": args.sigma_z,
+        "sigma_0": args.sigma_0,
     }
     make_plot(args.compas_h5, args.out, cosmic_kwargs)
 
